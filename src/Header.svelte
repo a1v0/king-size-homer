1cd7821d6bd8032ec07440874296a6c702c33b9d
avc285 <avc285@gmail.com>
2023-03-28 21:09:12 +0100

    create Header and Footer components

---
+++ src/Header.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+    let boobs = "howdy, boobies!";
+</script>
+
+<header>
+    <h1>{boobs}</h1>
+</header>
+
+<style>
+    header {
+        background-color: orange;
+    }
+</style>
