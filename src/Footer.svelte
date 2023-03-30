43ee0ca110fdc29210df4be87c6762bc94c7f5fc
avc285 <avc285@gmail.com>
2023-03-30 21:23:00 +0100

    add joke to Footer

--- src/Footer.svelte
+++ src/Footer.svelte
@@ -1,6 +1,9 @@
 <footer>
     <h2>Footer</h2>
-    <p>Copyright &copy; 2023 <a href="https://github.com/a1v0">a1v0</a>.</p>
+    <p>
+        Copyright &copy; 2023 <a href="https://github.com/a1v0">a1v0</a>. Not
+        affiliated with Compu-Global-Hyper-Mega-Net.
+    </p>
 </footer>
 
 <style>
